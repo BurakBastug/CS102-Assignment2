6137c1af78d54dbed6eb0dcc109a81c511983a92
Arda <ardaoztrk@yahoo.com>
2022-09-21 18:47:40 +0300

    Menü eklendi arkidişler

--- assignment2.java
+++ assignment2.java
@@ -1,5 +1,6 @@
 
 import java.util.Arrays;
+import java.util.Scanner;
     
     
 /**
@@ -75,6 +76,70 @@ public class assignment2 {
             }
         }
         return sum;
+    }
+
+    public static int[] initArray(int size) {
+        int[] arr = new int[size];
+
+        for(int i=0; i<size; i++) {
+            int rand = (int)(Math.random() * 101);
+            arr[i] = rand;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        int size;
+        int[] randArr;
+
+        System.out.print("Inıtialize a new random array(0 to 100) with given size: ");
+        size = scan.nextInt();
+        randArr = initArray(size);
+
+        do{
+            System.out.println("(1) Find the mininmum of the array");
+            System.out.println("(2) Find the maximum of the array");
+            System.out.println("(3) Find the average of the array");
+            System.out.println("(4) Display how each element of the array differs from the average");
+            System.out.println("(5) Find the sum of elements with odd indexes");
+            System.out.println("(6) Find the sum of elements with even indexes");
+            System.out.println("(7) Display the array");
+            System.out.println("(8) Exit");
+
+            System.out.print("Enter a choice: ");
+            choice = scan.nextInt();
+
+            if(choice<1 || choice>8){
+                System.out.println("Please enter a valid choice!");
+            }
+            else if(choice == 1){
+                System.err.println("The minimum is: " + findMin(randArr));
+            }
+            else if(choice == 2){
+                System.err.println("The maximum is: " + findMax(randArr));
+            }
+            else if(choice == 3){
+                System.err.println("The average is: " + findAverage(randArr));
+            }
+            else if(choice == 4){
+                System.err.println("Elements' difference from average is: " + Arrays.toString(findDifferenceFromAverage(randArr)));
+            }
+            else if(choice == 5){
+                System.err.println("The sum of elements with odd indexes is: " + sumOdd(randArr));
+            }
+            else if(choice == 6){
+                System.err.println("The sum of elements with even indexes: " + sumEven(randArr));
+            }
+            else if(choice == 7){
+                System.err.println(Arrays.toString(randArr));
+            }
+        }
+        while(choice != 8);
+
+        scan.close();
+        System.out.println("Goodbye!");
     }
 }
 
