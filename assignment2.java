b92e5d01e11dd0913ba48de3ec4ec0743220d73b
Burak Baştuğ <99401604+BurakBastug@users.noreply.github.com>
2022-09-21 15:11:48 +0300

    Added function shows difference from average

--- assignment2.java
+++ assignment2.java
@@ -1,1 +1,36 @@
+import java.util.Arrays;
 
+/**
+ * assignment2
+ */
+public class assignment2 {
+
+    public static double[] findDifferenceFromAverage( int arr[] ) {
+        double average=0;
+        int sum=0;
+        double differenceArr[]=new double [arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum+arr[i];            
+        }
+        average = sum/arr.length;
+
+        for (int i = 0; i < arr.length; i++) {
+            differenceArr[i] = (int) (arr[i]-average);
+        }
+        return differenceArr;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,10,15,20,25};
+
+        System.out.println(Arrays.toString(findDifferenceFromAverage(arr)));
+
+
+
+
+
+
+
+ }
+}
