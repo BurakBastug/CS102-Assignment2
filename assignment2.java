4b19e1bb350b58d2d22a89ebbde78fb3d7ca38c5
Berkin Kağan Ateş <kagan.ates@ug.bilkent.edu.tr>
2022-09-21 15:28:42 +0300

    MaxMin
    Main deleted

--- assignment2.java
+++ assignment2.java
@@ -4,14 +4,6 @@
 
 public class assignment2 {
 
-    public static void main(String[] args) {
-        int arr[] = {5,2,4,7,8};
-        System.out.println(findMin(arr));
-        System.out.println(findMax(arr));
-        
-
-        
-    }
     public static int findMin(int[] arr){
         int min = 101;
         for(int i = 0; i < arr.length; i++){
